3d5c3f810803fbe4fd8fb5868e58cdba426da2bd
motoakioi <oi@comp.sd.keio.ac.jp>
2019-01-11 13:48:21 +0900

    [add] send message 'UDP GO' via udp

---
+++ txUdp/txUdp.go
@@ -0,0 +1,43 @@
+package main 
+
+import(
+	"fmt"
+	"log"
+	"os"
+	"net"
+)
+
+func main (){
+
+	// Check Arguments
+	if len(os.Args) != 3{
+		argErMsg := string("Usage: " + os.Args[0] + "[IP] [PORT]")
+		log.Fatal(argErMsg)
+	}
+
+	fmt.Println("Start")
+
+	// Get parameters from arguments
+	ip := os.Args[1]
+	port := os.Args[2]
+	dst := string(ip + ":" + port)
+	fmt.Println("Sending to " + dst)
+
+	// Connect to server
+	conn, erConn := net.Dial("udp", dst)
+	// In case of error
+	if erConn != nil {
+		log.Fatal("Error: net.Dial function.")
+	}
+	defer conn.Close()
+
+	// Set message
+	msg := []byte("UDP GO")
+
+	// Send message
+	_, erWrite := conn.Write(msg)
+	if erWrite != nil{
+		log.Fatal("Error: Can NOT send message.")
+	}
+
+}
